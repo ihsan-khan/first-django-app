2ab9088ed6dd7ac9f7c7f76908f791b2bf110be0
ihsan-khan <ehsankhan0577@gmail.com>
2025-10-17 15:20:39 +0500

    update: Adding related objects

--- polls/admin.py
+++ polls/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from .models import Question,Choice
 
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
 class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
     fields = ('pub_date', 'question_text')
 
 admin.site.register(Question, QuestionAdmin)
