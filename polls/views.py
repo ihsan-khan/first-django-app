2375c22dd731e004ff67aa279369ef21a58ffc95
ihsan-khan <ehsankhan0577@gmail.com>
2025-10-17 14:52:49 +0500

    writing form and submitting vote

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,10 @@
 from unittest import loader
+from urllib import request
 from django.shortcuts import get_object_or_404, render
 from django.http import Http404, HttpResponse
+from django.db.models import F
 
-from polls.models import Question
+from polls.models import Choice, Question
 
 # Create your views here.
 def index(request):
@@ -20,7 +22,20 @@ def detail(request, question_id):
     # return render(request, "polls/detail.html", {"question": question})
 
 def results(request, question_id):
-    return render(request, "polls/results.html", {"question_id": question_id})
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/results.html", {"question": question})
 
 def vote(request, question_id):
-    return HttpResponse(f"You're voting on question {question_id}.")
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, "polls/detail.html", {
+            "question": question,
+            "error_message": "You didn't select a choice."
+        })
+    else:
+        selected_choice.votes = F("votes") + 1
+        selected_choice.save()
+        return HttpResponse(f"You're voting on question {question_id}.")
